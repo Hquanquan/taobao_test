44c7bd8d50331227c6795d40d40d5911000c67f4
Hquanquan <14320479302@qq.com>
2020-08-05 16:48:48 +0800

    最新的

--- testsuits/test.py
+++ testsuits/test.py
@@ -1,14 +1,8 @@
-
-# -*- coding:utf-8 -*-
+# coding = utf-8
 # import unittest
 from selenium import webdriver
-
 driver = webdriver.Chrome(r'E:\python\chromedriver.exe')
-# driver = webdriver.Chrome(chrome_options=chrome_options)
-
 driver.implicitly_wait(10)
-
 driver.maximize_window()
-
 driver.get('https://www.taobao.com/')
 
